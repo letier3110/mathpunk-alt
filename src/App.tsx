bf9ddcbd4b9f41c7cdc7a682985b9344e90134f8
letier3110 <letier3110@gmail.com>
2022-12-11 20:02:31 +0400

    add rerolls

--- src/App.tsx
+++ src/App.tsx
@@ -178,11 +178,11 @@ enum DIFFICULTIES {
 }
 
 interface DifficultySettings {
-  minTargetValue: number;
-  maxTargetValue: number;
-  minNumenatorValue: number;
-  maxNumenatorValue: number;
-  preciseness: number;
+  minTargetValue: number
+  maxTargetValue: number
+  minNumenatorValue: number
+  maxNumenatorValue: number
+  preciseness: number
 }
 
 const VALUES: Record<DIFFICULTIES, DifficultySettings> = {
@@ -191,31 +191,31 @@ const VALUES: Record<DIFFICULTIES, DifficultySettings> = {
     maxTargetValue: 15,
     minNumenatorValue: 1,
     maxNumenatorValue: 20,
-    preciseness: 50,
+    preciseness: 50
   },
   [DIFFICULTIES.HARD]: {
     minTargetValue: 2000,
     maxTargetValue: 8000,
     minNumenatorValue: 100,
     maxNumenatorValue: 999,
-    preciseness: 25,
+    preciseness: 25
   }
 }
 
 let latestTarget = 0
 
 const generateTarget = (hardMode = false): number => {
-  const mode = hardMode ? DIFFICULTIES.HARD : DIFFICULTIES.EASY;
-  const min = VALUES[mode].minTargetValue;
-  const max = VALUES[mode].maxTargetValue;
+  const mode = hardMode ? DIFFICULTIES.HARD : DIFFICULTIES.EASY
+  const min = VALUES[mode].minTargetValue
+  const max = VALUES[mode].maxTargetValue
   latestTarget = Math.floor(Math.random() * (max - min) + min)
   return latestTarget
 }
 
 const generateNumenator = (hardMode = false): number => {
-  const mode = hardMode ? DIFFICULTIES.HARD : DIFFICULTIES.EASY;
-  const min = VALUES[mode].minNumenatorValue;
-  const max = VALUES[mode].maxNumenatorValue;
+  const mode = hardMode ? DIFFICULTIES.HARD : DIFFICULTIES.EASY
+  const min = VALUES[mode].minNumenatorValue
+  const max = VALUES[mode].maxNumenatorValue
   let res = Math.floor(Math.random() * (max - min) + min)
   while (res === latestTarget) {
     res = Math.floor(Math.random() * (max - min) + min)
@@ -242,7 +242,7 @@ function App() {
   const [hardMode, setHardMode] = useState<boolean>(false)
   const [count, setCount] = useState(generateTarget())
   const [tutorialMode, setTutorialMode] = useState<boolean>(false)
-  const [hp, setHp] = useState(10)
+  const [left, setLeft] = useState(3)
   const [enemyHp, setEnemyHp] = useState(10)
   const [round, setRound] = useState<number>(1)
   const [chain, setChain] = useState<CardType[]>([])
@@ -260,8 +260,8 @@ function App() {
   }, [chain])
 
   const isGameEnded = enemyHp <= 0
-  const mode = hardMode ? DIFFICULTIES.HARD : DIFFICULTIES.EASY;
-  const preciseness = VALUES[mode].preciseness;
+  const mode = hardMode ? DIFFICULTIES.HARD : DIFFICULTIES.EASY
+  const preciseness = VALUES[mode].preciseness
 
   const handleAddCard = ({ card, index }: AddCardProps) => {
     if (!index) {
@@ -291,6 +291,7 @@ function App() {
     setCount(generateTarget(hardMode))
     setRound(round + 1)
     setChain([])
+    setLeft(3)
     setDeck(getDeckPool(hardMode))
   }
 
@@ -300,6 +301,14 @@ function App() {
     setChain([])
     setDeck(getDeckPool(hardMode))
     setEnemyHp(10)
+    setLeft(3)
+  }
+
+  const handleReroll = () => {
+    if (left > 0) {
+      setLeft(left - 1)
+      setDeck(getDeckPool(hardMode))
+    }
   }
 
   const handleEqual = () => {
@@ -308,8 +317,8 @@ function App() {
       handleStartNewRound()
       return
     }
-    const mode = hardMode ? DIFFICULTIES.HARD : DIFFICULTIES.EASY;
-    const preciseness = VALUES[mode].preciseness / 100;
+    const mode = hardMode ? DIFFICULTIES.HARD : DIFFICULTIES.EASY
+    const preciseness = VALUES[mode].preciseness / 100
     if (equalizerResult > count * (1 - preciseness) && equalizerResult < count * (1 + preciseness)) {
       const res = Math.round((1 - Math.abs((equalizerResult - count) / count)) * 5)
       console.log(res)
@@ -320,121 +329,123 @@ function App() {
     handleStartNewRound()
   }
 
-  // console.log('value', tutorialMode)
-
   return (
-    <div className='root'>
-      {tutorialMode && (<div className='sidebar'>
+    <>
+      <div className='root'>
+        {tutorialMode && (
+          <div className='sidebar'>
+            <div>
+              1. Select and click card from the bottom of the screen. Bottom of the screen is represents your HAND.
+            </div>
+            <div>2. Combine your cards to achieve value as close as you can to the target.</div>
+            <div>3. In order to win, you need to make correct guesses and lower points to zero.</div>
+            <div>
+              4. Your value must consist {100 - preciseness}% of target's value. Otherwise points will grow, pushing
+              away victory of the game.
+            </div>
+            <div>5. To confirm your value - click on the card after equals (=) sign.</div>
+          </div>
+        )}
         <div>
-          1. Select and click card from the bottom of the screen. Bottom of the screen is represents your HAND.
+          Tutorial mode?
+          <input type='checkbox' value={tutorialMode + ''} onChange={(e) => setTutorialMode(e.target.checked)} />
+          {/* {tutorialMode && <div className='tutorialText'>LABELS WITH THIS FONT ARE FOR TUTORIAL</div>} */}
         </div>
-        <div>
-          2. Combine your cards to achieve value as close as you can to the target. 
-        </div>
-        <div>
-          3. In order to win, you need to make correct guesses and lower points to zero.
-        </div>
-        <div>
-          4. Your value must consist {100 - preciseness}% of target's value. Otherwise points will grow, pushing away victory of the game.
-        </div>
-        <div>
-          5. To confirm your value - click on the card after equals (=) sign. 
-        </div>
-      </div>)}
-      <div>
-        Tutorial mode?
-        <input type='checkbox' value={tutorialMode + ''} onChange={(e) => setTutorialMode(e.target.checked)} />
-        {/* {tutorialMode && <div className='tutorialText'>LABELS WITH THIS FONT ARE FOR TUTORIAL</div>} */}
-      </div>
-      <div className='hps'>
-        {/* <div>
+        <div className='hps'>
+          {/* <div>
           Your points:
           {hp}
         </div> */}
-        {/* <div>
+          {/* <div>
           Round:
           {round}
         </div> */}
-        <div>
-          Remaining points to beat:
-          {enemyHp}
+          <div>
+            Remaining points to beat:
+            {enemyHp}
+          </div>
         </div>
-      </div>
-      {isGameEnded === false && (
-        <>
-          <div className='count'>{count}</div>
-          {chain.length > 0 && (
-            <div className='chain'>
-              {chain.map((x) => {
-                return (
-                  <div key={x.getId()} className='chainElem'>
-                    <div onClick={() => handleRemoveCard({ card: x })} className='card noAddition'>
-                      <div className='mainText'>{x.getCount()}</div>
-                      <div className={['addition', x.getDescription()].join(' ')}>
+        {isGameEnded === false && (
+          <>
+            <div className='count'>{count}</div>
+            {chain.length > 0 && (
+              <div className='chain'>
+                {chain.map((x) => {
+                  return (
+                    <div key={x.getId()} className='chainElem'>
+                      <div onClick={() => handleRemoveCard({ card: x })} className='card noAddition'>
+                        <div className='mainText'>{x.getCount()}</div>
+                        <div className={['addition', x.getDescription()].join(' ')}>
+                          <div className='additionText'>{x.getName()}</div>
+                        </div>
+                      </div>
+                      <div className={['cardAddition', x.getDescription()].join(' ')}>
                         <div className='additionText'>{x.getName()}</div>
                       </div>
                     </div>
-                    <div className={['cardAddition', x.getDescription()].join(' ')}>
+                  )
+                })}
+                {chain.length > 0 && (
+                  <div className='chainResultElem'>
+                    <div className={['cardAddition', 'equalizer'].join(' ')}>
+                      <div className='additionText'>=</div>
+                    </div>
+                    <div onClick={handleEqual} className='card'>
+                      <div className='mainText'>
+                        {equalizerResult.toString().indexOf('.') >= 0 ? equalizerResult.toFixed(2) : equalizerResult}
+                      </div>
+                    </div>
+                  </div>
+                )}
+              </div>
+            )}
+            {/* {tutorialMode && chain.length > 0 && (<div className='tutorialText'>CLICK ON THE CARD RESULT CARD TO APPLY IT</div>)} */}
+            <div className='cards'>
+              {deck.map((x, index) => {
+                const translateY = Math.abs(-Math.floor(deck.length / 2) + index)
+                const rotate = -Math.floor(deck.length / 2)
+                return (
+                  <div
+                    key={x.getId()}
+                    className='card'
+                    style={{
+                      rotate: `${(rotate + index) * 10}deg`,
+                      translate: `0px ${translateY * translateY * 12}px`
+                    }}
+                    onClick={() => handleAddCard({ card: x })}
+                  >
+                    <div className='mainText'>{x.getCount()}</div>
+                    <div className={['addition', x.getDescription()].join(' ')}>
                       <div className='additionText'>{x.getName()}</div>
                     </div>
                   </div>
                 )
               })}
-              {chain.length > 0 && (
-                <div className='chainResultElem'>
-                  <div className={['cardAddition', 'equalizer'].join(' ')}>
-                    <div className='additionText'>=</div>
-                  </div>
-                  <div onClick={handleEqual} className='card'>
-                    <div className='mainText'>
-                      {equalizerResult.toString().indexOf('.') >= 0 ? equalizerResult.toFixed(2) : equalizerResult}
-                    </div>
-                  </div>
-                </div>
-              )}
             </div>
-          )}
-          {/* {tutorialMode && chain.length > 0 && (<div className='tutorialText'>CLICK ON THE CARD RESULT CARD TO APPLY IT</div>)} */}
-          <div className='cards'>
-            {deck.map((x, index) => {
-              const translateY = Math.abs(-Math.floor(deck.length / 2) + index)
-              const rotate = -Math.floor(deck.length / 2)
-              return (
-                <div
-                  key={x.getId()}
-                  className='card'
-                  style={{
-                    rotate: `${(rotate + index) * 10}deg`,
-                    translate: `0px ${translateY * translateY * 12}px`
-                  }}
-                  onClick={() => handleAddCard({ card: x })}
-                >
-                  <div className='mainText'>{x.getCount()}</div>
-                  <div className={['addition', x.getDescription()].join(' ')}>
-                    <div className='additionText'>{x.getName()}</div>
-                  </div>
-                </div>
-              )
-            })}
+            {/* {tutorialMode && deck.length > 0 && <div className='tutorialText'>CLICK ON THE CARD TO PLAY IT</div>} */}
+          </>
+        )}
+        {isGameEnded === true && (
+          <div className='win'>
+            <div>🥳🥳🥳</div>
+            <div>You won!</div>
+            <div>
+              Hard mode?
+              <input type='checkbox' value={hardMode + ''} onChange={(e) => setHardMode(!hardMode)} />
+            </div>
+            {tutorialMode && <div className='tutorialText'>PRESS THE CARD TO START NEW GAME</div>}
+            <div className='card' onClick={handleStartNewGame}>
+              Start new game?
+            </div>
           </div>
-          {/* {tutorialMode && deck.length > 0 && <div className='tutorialText'>CLICK ON THE CARD TO PLAY IT</div>} */}
-        </>
-      )}
-      {isGameEnded === true && (
-        <div className='win'>
-          <div>🥳🥳🥳</div>
-          <div>You won!</div>
-          <div>
-            Hard mode?
-            <input type='checkbox' value={hardMode + ''} onChange={(e) => setHardMode(!hardMode)} />
-          </div>
-          {tutorialMode && <div className='tutorialText'>PRESS THE CARD TO START NEW GAME</div>}
-          <div className='card' onClick={handleStartNewGame}>
-            Start new game?
-          </div>
+        )}
+      </div>
+      <div className='reroll'>
+        <div className={['card', left <= 0 ? 'cardDisabled' : ''].join(' ')} onClick={handleReroll}>
+          reroll? {left} left
         </div>
-      )}
-    </div>
+      </div>
+    </>
   )
 }
 
