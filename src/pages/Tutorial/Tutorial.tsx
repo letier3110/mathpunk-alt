412e6ee1e36974a67d1791e39dd10213de1d1d6a
letier3110 <letier3110@gmail.com>
2023-01-09 21:18:29 +0400

    fix: tutorial dnd

--- src/pages/Tutorial/Tutorial.tsx
+++ src/pages/Tutorial/Tutorial.tsx
@@ -335,8 +335,11 @@ export const Tutorial: FC<TutorialProps> = () => {
                           card={chain[i]}
                           noAddition
                           className={['card noAddition'].join(' ')}
-                          handleCardClick={() => handleRemoveCard({ card: chain[i], index: i })}
-                          // handleMouseDown={() => handleRemoveCard({ card: chain[i], index: i })}
+                          // handleCardClick={() => handleRemoveCard({ card: chain[i], index: i })}
+                          handleMouseDown={() => {
+                            handleRemoveCard({ card: chain[i], index: i })
+                            setSelectedCard((prev) => (prev?.getId() === chain[i].getId() ? null : chain[i]))
+                          }}
                         />
                       </Fragment>
                     )
